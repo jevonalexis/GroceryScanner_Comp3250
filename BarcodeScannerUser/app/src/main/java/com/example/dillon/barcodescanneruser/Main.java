2ede60d6f66b3db2b578f87658bd76a6ecb50030
dillonbhola <dillonbhola@gmail.com>
2015-03-08 20:10:09 -0400

    User App Skeleton

---
+++ BarcodeScannerUser/app/src/main/java/com/example/dillon/barcodescanneruser/Main.java
@@ -0,0 +1,73 @@
+package com.example.dillon.barcodescanneruser;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dillon.barcodescanneruser.ZXing.IntentIntegrator;
+import com.example.dillon.barcodescanneruser.ZXing.IntentResult;
+
+
+public class Main extends ActionBarActivity {
+    Button scan;
+    TextView code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scan = (Button)findViewById(R.id.btn_scan);
+        code = (TextView)findViewById(R.id.tv_code);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void scanBarcode(View view) {
+        IntentIntegrator scanIntegrator = new IntentIntegrator(Main.this);
+        scanIntegrator.initiateScan();
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        String scanContent="";
+        if (scanningResult != null) {
+            scanContent = scanningResult.getContents();
+            code.setText(scanContent);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
